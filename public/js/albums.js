d042a40eea70c22b7104224ad7c4f5de0e095052
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2022-11-10 11:59:15 -0300

    deploy arrangement

--- public/js/albums.js
+++ public/js/albums.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector(".albums__cells")
 
-const albums = [
+const albumsInfo = [
     {title:"Blurryface",poster:"../src/img/blurryface.png"},
     {title:"Trench",poster:"../src/img/trench.png"},
     {title:"Twenty One Pilots",poster:"../src/img/twentyonepilots.png"},
@@ -10,24 +10,26 @@ const albums = [
 ]
 
 function printAlbums(){
-    for(let index in albums){
+    for(let index in albumsInfo){
         const albumContainer = document.createElement("div")
         const albumInfo = document.createElement("div");
         const albumTitle = document.createElement("h5");
         albumInfo.className="album__info"
         albumTitle.className="album__title"
-        albumTitle.innerHTML=albums[index].title;
+        albumTitle.innerHTML=albumsInfo[index].title;
         albumInfo.appendChild(albumTitle)
-        albumContainer.style.background=`url(${albums[index].poster})`
+        albumContainer.style.background=`url(${albumsInfo[index].poster})`
         albumContainer.style.backgroundSize="cover"
 
         albumContainer.className="album__cell"
         
         wrapper.appendChild(albumContainer)
         albumContainer.appendChild(albumInfo)
+// const anchor = document.createElement("a")
+// anchor.href="public/html/"+albumsInfo[index].title.toLocaleLowerCase()
 
-
-        albumContainer.onclick = () => window.location.href=`http://127.0.0.1:5500/public/html/${albums[index].title.toLocaleLowerCase()}.html`
+        albumContainer.onclick = () => window.location.href=`${albumsInfo[index].title.toLocaleLowerCase()}.html`
+        console.log("TESTE!");
 
     }
 }
